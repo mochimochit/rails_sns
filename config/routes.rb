8933e55cd929415e2b6d66f3508e9e80ff9e71f7
mochimochit <plain_4362@yahoo.co.jp>
2022-11-01 15:05:39 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/' => 'home#top'
+  get 'login' => 'home#login'
+  get 'plof' => 'home#prof'
+  get 'post' => 'home#post'
+  get 'postlost' => 'home#postlist'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
